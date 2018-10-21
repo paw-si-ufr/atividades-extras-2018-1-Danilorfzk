c6fe314b8311e65f7ff392b95aff1565c6ebadfe
Danilorfzk <31334241+Danilorfzk@users.noreply.github.com>
2018-10-21 02:08:32 -0200

    Atividade_6
    Atividade_6

---
+++ Atividade_6/meuScript.js
@@ -0,0 +1,9 @@
+function pausaVideo(){
+    var v = document.getElementById("videoid");
+    v.pause();
+}
+
+function pausaAudio(){
+    var a = document.getElementById("audio");
+    a.pause();
+}
